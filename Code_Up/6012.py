60f609e4dd8517076cf59d230f8dc60f0b7091d5
heedahan16 <devhee.handa@gmail.com>
2023-10-11 08:55:40 +0900

    6012 : [기초-입출력] 정수 2개 입력받아 그대로 출력하기1(설명)(py)

---
+++ Code_Up/6012.py
@@ -0,0 +1,18 @@
+# 6012 : [기초-입출력] 정수 2개 입력받아 그대로 출력하기1(설명)(py)
+
+# 줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+# 예시
+# a = input() 
+# b = input()
+# a=int(a)
+# b=int(b)
+# print(a)
+# print(b)
+# 과 같은 방법으로 가능하다.
+
+x = input()
+y = input()
+x = int(x)
+y = int(y)
+print(x)
+print(y)
