76747944b90c312d9d982dcb4e200a52feb5ddc4
heedahan16 <devhee.handa@gmail.com>
2023-10-20 09:58:54 +0900

    6058 : [기초-논리연산] 둘 다 거짓일 경우만 참 출력하기(py)

---
+++ Code_Up/6058.py
@@ -0,0 +1,13 @@
+# 6058 : [기초-논리연산] 둘 다 거짓일 경우만 참 출력하기(py)
+
+# 2개의 정수값이 입력될 때,
+# 그 불 값(True/False) 이 모두 False 일 때에만 True 를 출력하는 프로그램을 작성해보자.
+# 입력
+# 2개의 정수가 공백을 두고 입력된다.
+# 출력
+# 두 값의 True / False 값이 모두 False 일 때만 True 를 출력하고, 그 외의 경우에는 False 를 출력한다.
+
+a, b = map(int, input().split())
+c, d = bool(a), bool(b)
+
+print(not(c) and not(d))
